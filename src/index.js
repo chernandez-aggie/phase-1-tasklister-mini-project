bc84e928508a33d7794eaf36520a1cf2133e32d7
candacehernandez <chernandez.aggie@gmail.com>
2022-02-23 23:17:04 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+//   Deliverables
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+//Ensure HTML file has finished loading before executing
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  buildToDo(e.target.new_task_description.value);
+  form.reset();
+
+})
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector('#list').appendChild(p);
+}
+
+function handleDelete(e){
+e.target.parentNode.remove();
+}
